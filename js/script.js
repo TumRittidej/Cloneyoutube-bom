2acc54f4794b6f07e57d854fcf20ac2eb60cffdf
TumRittidej <w.rittidej@gmail.com>
2021-08-07 21:23:05 +0700

    Succuss!

--- js/script.js
+++ js/script.js
@@ -15,9 +15,13 @@ function toggleMenu() {
     const closeMenu = document.querySelector('nav.sidebar')
     const closeMenuicon = document.querySelector('.sidebar-icon')
     const opensidebar = document.querySelector('nav.sidebar')
+    const slidecategory = document.querySelector('.category-menu-items')
+    const vdocontentslide = document.querySelector('.vdocontent')
 
     openMenu.classList.toggle('showsidebar-icon')
     closeMenu.classList.toggle('hidesidebar')
     closeMenuicon.classList.toggle('hidesidebar-icon')
     opensidebar.classList.toggle('showsidebarwidth1300px')
+    slidecategory.classList.toggle('slidecategorymenu')
+    vdocontentslide.classList.toggle('vdoslide')
 }
